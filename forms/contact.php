08ae0ac1eb845cd15aad982c023f94860f80c52d
Mher <mshakhkyan@gmail.com>
2026-03-09 13:39:48 +0400

    Email Configs

--- forms/contact.php
+++ forms/contact.php
@@ -7,7 +7,7 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'mher.shakhkyan@digitain.com';
+  $receiving_email_address = 'mshakhkyan@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
@@ -24,14 +24,14 @@
   $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
+  
   $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
+    'host' => 'smtp-relay.brevo.com',
+    'username' => 'a460b0001@smtp-brevo.com',
+    'password' => 'xsmtpsib-27a732a279dc08b037b68e8be65fdd51a65a445c1a7cb037c24bce519e79fd27-RSrey1OivA41lU83',
     'port' => '587'
   );
-  */
+  
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
